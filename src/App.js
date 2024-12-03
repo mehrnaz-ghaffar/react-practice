1dad4f8cd00d00a29bf4c46f22ae24457e8de9bd
mehrnaz <ghaffar@citynet.ir>
2024-12-03 10:02:24 +0330

    update: learned event handling in react

--- src/App.js
+++ src/App.js
@@ -7,6 +7,14 @@ const messages = [
 export default function App() {
   const step = 1;
 
+  function handlePrevious() {
+    alert("previous");
+  }
+
+  function handleNext() {
+    alert("next");
+  }
+
   return (
     <div className="steps">
       <div className="numbers">
@@ -20,10 +28,16 @@ export default function App() {
       </p>
 
       <div className="buttons">
-        <button style={{ backgroundColor: "#7950f2", color: "#fff" }}>
+        <button
+          onClick={handlePrevious}
+          style={{ backgroundColor: "#7950f2", color: "#fff" }}
+        >
           Previous
         </button>
-        <button style={{ backgroundColor: "#7950f2", color: "#fff" }}>
+        <button
+          onClick={handleNext}
+          style={{ backgroundColor: "#7950f2", color: "#fff" }}
+        >
           Next
         </button>
       </div>
