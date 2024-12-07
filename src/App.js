6f6cc9d223dfab0d3db388aa0ee5d6babedb200f
mehrnaz <ghaffar@citynet.ir>
2024-12-07 17:47:37 +0330

    chore: rendering list conditionally

--- src/App.js
+++ src/App.js
@@ -1,3 +1,8 @@
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: true },
+];
+
 function Logo() {
   return <h1> Far Away </h1>;
 }
@@ -11,7 +16,27 @@ function Form() {
 }
 
 function PackingList() {
-  return <div className="list">List</div>;
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item} />
+        ))}
+      </ul>
+      ;
+    </div>
+  );
+}
+
+function Item({ item }) {
+  return (
+    <li>
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {item.quantity} {item.description}
+      </span>
+      <button>X</button>
+    </li>
+  );
 }
 
 function Stats() {
