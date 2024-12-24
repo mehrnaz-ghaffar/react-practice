5f90bacb1872930c7b26d0f0df4d00c00c8a4c3b
mehrnaz <ghaffar@citynet.ir>
2024-12-24 17:20:04 +0330

    update: completed accordion phase one

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import "./styles.css";
 
 const faqs = [
@@ -16,5 +17,41 @@ const faqs = [
 ];
 
 export default function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Accordion data={faqs} />
+    </div>
+  );
+}
+
+function Accordion({ data }) {
+  return (
+    <div className="accordion">
+      {data.map((question, index) => (
+        <AccordionItem
+          number={index}
+          title={question.title}
+          text={question.text}
+          key={index}
+        />
+      ))}
+    </div>
+  );
+}
+
+function AccordionItem({ number, title, text }) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  function handleToggle() {
+    setIsOpen((isOpen) => !isOpen);
+  }
+
+  return (
+    <div className={` item ${isOpen ? "open" : ""}`} onClick={handleToggle}>
+      <p className="number">{number < 9 ? `0${number + 1}` : number + 1}</p>
+      <p className="title">{title}</p>
+      <p className="icon">{isOpen ? "-" : "+"}</p>
+      {isOpen && <p className="content-box">{text}</p>}
+    </div>
+  );
 }
