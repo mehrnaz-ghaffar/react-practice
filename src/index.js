001eeb9f0fba7a1b66c2a8992c6ab28000959024
mehrnaz <ghaffar@citynet.ir>
2025-01-26 17:55:17 +0330

    update:added star component

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+// import App from "./App";
+// import "./index.css";
+import StarRating from "./StarRating";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <StarRating />
   </React.StrictMode>
 );
