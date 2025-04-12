417fa3edd895593f0111407dc17f0b7bd72cecfb
mehrnaz <ghaffar@citynet.ir>
2025-04-12 17:38:47 +0330

    chore: load movie details

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import StarRating from "./StarRating";
 
 const average = (arr) =>
   arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
@@ -191,13 +192,81 @@ function Movie({ movie, onSelectMovie }) {
 // }
 
 function MovieDetails({ selectedId, onCloseMovie }) {
-  return (
-    <div>
-      <button className="btn-back" onClick={onCloseMovie}>
-        ←
-      </button>
+  const [movie, setMovie] = useState({});
+  const [loading, setLoading] = useState(false);
 
-      {selectedId}
+  const {
+    Title: title,
+    Year: year,
+    Poster: poster,
+    Runtime: runtime,
+    imdbRating,
+    Plot: plot,
+    Released: released,
+    Actors: actors,
+    Director: director,
+    Genre: genre,
+  } = movie;
+
+  useEffect(
+    function () {
+      async function getMovieDetails() {
+        setLoading(true);
+        const res = await fetch(
+          `http://www.omdbapi.com/?apikey=${KEY}&i=${selectedId}`
+        );
+
+        const data = await res.json();
+        setMovie(data);
+        setLoading(false);
+      }
+
+      getMovieDetails();
+    },
+
+    [selectedId]
+  );
+
+  return (
+    <div className="details">
+      {loading ? (
+        <Loader />
+      ) : (
+        <>
+          <header>
+            <button className="btn-back" onClick={onCloseMovie}>
+              ←
+            </button>
+
+            <img src={poster} alt={`Poster of ${movie} movie`} />
+
+            <div className="details-overview">
+              <h2>{title}</h2>
+
+              <p>
+                {released} &bull; {runtime}
+              </p>
+              <p>{genre}</p>
+              <p>
+                <span>⭐</span>
+                {imdbRating} IMDB rating
+              </p>
+            </div>
+          </header>
+
+          <section>
+            <div className="rating">
+              <StarRating maxRating={10} size={24} />
+            </div>
+
+            <p>
+              <em>{plot}</em>
+            </p>
+            <p> Starring {actors} </p>
+            <p> Directed by {director} </p>
+          </section>
+        </>
+      )}
     </div>
   );
 }
